47dcc66c74c58ec93aff330b636cffb3e8382fbf
joosthb <joost@hendricksen.net>
2022-01-21 16:13:34 +0100

    qr code image reader added

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
-from typing import Optional
-from fastapi import FastAPI
+from typing import Optional, List
+from fastapi import File, UploadFile, FastAPI
+# for QR image parsing
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+# for totp generation
 import mintotp
 
 app = FastAPI()
@@ -18,3 +23,10 @@ def read_item(item_id: int, q: Optional[str] = None):
 async def read_item(shared_key: str):
     return {"current_code": mintotp.totp(shared_key)}
 
+@app.post("/upload")
+async def upload(file: UploadFile = File(...)):
+    # extract qr data
+    contents = await file.read()
+    # save_file(file.filename, contents)
+    qrdata += decode(Image.frombytes(contents))
+    return {"qr_data": qrdata}
