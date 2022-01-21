cc5e1a8eb10a1b707f1828feacfa0ffdbe901f4e
joosthb <joost@hendricksen.net>
2022-01-21 15:55:56 +0100

    first totp generator implemented

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # onlineauthenticator
 Source code repo of onlineauthenticator.com
+
+local build
+`docker build -t onlineauthenticator .`
+
+local run
+`docker run --rm -p 80:80 onlineauthenticator`
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from typing import Optional
-
 from fastapi import FastAPI
+import mintotp
 
 app = FastAPI()
 
@@ -13,3 +13,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/code/{shared_key}")
+async def read_item(shared_key: str):
+    return {"current_code": mintotp.totp(shared_key)}
+
